5545ab54390d85a1e68393ea2179107e7c694b05
Daniel Maldonado <daniel_dlcmaldonado@hotmail.com>
2024-04-18 16:03:41 -0600

    (+) npm-parImpar

---
+++ 15-JS-npm/npm-parImpar/index.js
@@ -0,0 +1,6 @@
+//Importar la función desde la ruta específica
+import determinarParidad from "./modules/parImpar.js";
+
+//Utilizando mi función importada
+determinarParidad(236300);
+determinarParidad(89742);
